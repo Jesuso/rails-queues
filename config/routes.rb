754ee55e3f1250540d4c1fc48d2480a153336e14
Nacho Ortiz <jesus.ortiz@apptegy.com>
2023-07-12 13:48:44 -0600

    Added karafka web UI

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
+require 'karafka/web'
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
-  require 'sidekiq/web'
   mount Sidekiq::Web => '/sidekiq'
+  mount Karafka::Web::App, at: '/karafka'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
